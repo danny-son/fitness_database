e47aca8d7f8ac5fe936bcc6e407c92f341b2d5b9
Danny Son <dannyson900@gmail.com>
2022-04-24 16:01:39 -0400

    Updated db  and procedures some tables pk now have autoincrement and examples of some tuples

--- database/fitness_db.sql
+++ database/fitness_db.sql
@@ -10,32 +10,46 @@ password VARCHAR(64) NOT NULL,
 reg_date DATE NOT NULL,
 login_streak INT NOT NULL);
 
+
 -- table for acheievements and subclasses representing the types of achievements
 CREATE TABLE achievements
 (a_id INT NOT NULL PRIMARY KEY,
 name VARCHAR(64) NOT NULL,
 description VARCHAR(500) NOT NULL,
 points INT NOT NULL);
+INSERT INTO achievements VALUES 
+(0, "Fresh Start", "Login once (automatically unlocked upon registration)", 100) ,
+(1, "A Dedicated Week", "Login 7 days consecutively", 100),
+(2, "A Month of Hard Work", "Login 30 days consecutively", 500),
+(3, "Happy Fitness Anniversary", "Login 365 days consecutively", 1000),
+(4, "Fitness Guru", "Login for 1000 days consecutively", 2500),
+(5, "Baby Steps", "Have a Workout Log that details a workout of at least 10 minutes", 100),
+(6, "Breaking a Sweat", "Have a Workout Log that details a workout of at least 30 minutes", 300),
+(7, "Getting With the Program", "Have a Workout Log that details a workout of at least an hour", 500),
+(8, "Getting Serious", "Have a Workout Log that details a workout of at least 2 hours", 1000);
+ 
 
 -- achievement subclass table
 CREATE TABLE login_streaks
 (a_id INT NOT NULL PRIMARY KEY,
 FOREIGN KEY (a_id) REFERENCES achievements(a_id)
 ON UPDATE CASCADE ON DELETE CASCADE);
+INSERT INTO login_streaks VALUES (0), (1), (2), (3), (4);
 
 -- achievement subclass table
 CREATE TABLE workout_time
 (a_id INT NOT NULL PRIMARY KEY,
 FOREIGN KEY (a_id) references achievements(a_id)
 ON UPDATE CASCADE ON DELETE CASCADE);
+INSERT INTO workout_time VALUES (5),(6),(7),(8);
 
 -- table for achievements unlocked by users
 
 CREATE TABLE user_achievements
-(achievement_unlock_id VARCHAR(24) NOT NULL PRIMARY KEY,
-user_id VARCHAR(24) NOT NULL,
+(achievement_unlock_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+user_id VARCHAR(64) NOT NULL,
 a_id INT NOT NULL,
-time_unlock DATETIME NOT NULL,
+date_unlock DATE NOT NULL,
 FOREIGN KEY (a_id) REFERENCES achievements(a_id)
 ON UPDATE CASCADE ON DELETE CASCADE,
 FOREIGN KEY (user_id) REFERENCES user_account(user_id)
@@ -44,12 +58,25 @@ ON UPDATE CASCADE ON DELETE CASCADE);
 -- table for workout 
 CREATE TABLE workout
 (w_id INT NOT NULL PRIMARY KEY,
-name VARCHAR(20) NOT NULL,
+name VARCHAR(64) NOT NULL,
 description VARCHAR(999) NOT NULL,
 equipment VARCHAR(999) NOT NULL,
-muscle_group VARCHAR(20) NOT NULL,
-difficulty VARCHAR(20) NOT NULL,
-exercise_type VARCHAR(20) NOT NULL);
+muscle_group VARCHAR(999) NOT NULL,
+difficulty INT NOT NULL, # 1 - 10 scale; 1 meaning easy for everyone, 10 meaning only experts should try this
+exercise_type VARCHAR(99) NOT NULL);
+
+
+-- examples of tuples for workout, the user will only be able to view workouts
+INSERT INTO workout VALUES 
+(2, "Squat", "Calisthenics exercise that starts from a standing position and is done by lowering the hip", "none", "glutes, hamstrings, quads", 1, "calisthenics"),
+(1, "Pull-up", "Calisthenics exercise that starts with the body suspended by holding onto a horizontal bar with the hands and done by pulling the body up so the chin is parallel with the bar", "pull-up bar", "abdominals. back, biceps, forearms, shoulders", 6, "calisthenics"),
+(0, "Pushup", "Calisthenics exercise starting from the prone position and done by raising and lowering the body using the arms", "none", "abdominals, deltoids, pectoral, triceps", 4, "calisthenics"),
+(3, "Plank", "Isometric exercise that starts at the prone position and is done by maintaining said position for a duration of time", "none", "abdominals, glutes, obliques", 2, "isometric"),
+(4, "One-arm concentration curl", "Isolated bicep curl that uses a dumbbell and is done while sitting", "dumbbell", "biceps. forearms", 3, "weight-training"),
+(5, "Barbell reverse curl", "Bicep curl that uses a barbell where the hands are holding the bar from on-top and is done while standing and by tucking the elbows towards the side of the torso while pulling the barbell towards the shoulders", "barbell", "biceps, forearms", 3, "weight-training"),
+(6, "Deadlift", "Exercise that uses a barbell, starts with barbell on the ground and is done by squatting and pulling the bar up to the hips using the legs and core", "barbell", "abdominals, glutes, hamstrings, quads, traps", 5 , "weight-training");
+
+
 
 /*
 CREATE TABLE equipment
@@ -65,7 +92,7 @@ ON UPDATE CASCADE ON DELETE CASCADE);
 
 -- table for log
 CREATE TABLE `log`
-(log_id INT NOT NULL PRIMARY KEY,
+(log_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 datetime DATETIME NOT NULL,
 description VARCHAR(500) NOT NULL,
 user_id VARCHAR(24) NOT NULL,
@@ -84,12 +111,23 @@ ON UPDATE CASCADE ON DELETE CASCADE);
 
 -- create table for meal
 CREATE TABLE meal
-(meal_id INT PRIMARY KEY,
+(meal_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 description VARCHAR(500) NOT NULL,
 total_calories INT NOT NULL,
 carbs_g INT NOT NULL,
 protein_g INT NOT NULL,
 fat_g INT NOT NULL); 
+
+-- examples of tuples for meal, the user will be able perform CRUD operation on meals
+INSERT INTO meal (description, total_calories, carbs_g, protein_g, fat_g) VALUES 
+("Banana oatmeal (1 cup oats) w/ low-fat milk (1 cup)", 490, 97, 7 ,19),
+("Tofu scramble", 385, 10, 29,28),
+("Egg-white omelet (4 egg whites, 1 whole egg) with spinach (1 cup) and oyster mushrooms (½ cup)", 235.4, 5.3, 26, 12.2),
+("Avocado tuna salad", 472, 19, 36, 31),
+("Chicken broccoli casserole", 492,39,22,37),
+("Chicken caprese sandwich", 275,8,34,11);
+
+
 
 -- subclass for log
 create table diet_log
@@ -99,6 +137,8 @@ FOREIGN KEY (log_id) REFERENCES `log`(log_id)
 ON UPDATE CASCADE ON DELETE CASCADE,
 FOREIGN KEY (meal_id) REFERENCES meal(meal_id)
 ON UPDATE CASCADE ON DELETE CASCADE);
+
+
 
 
 
--- database/fitness_functions_procedures.sql
+++ database/fitness_functions_procedures.sql
@@ -7,7 +7,9 @@ BEGIN
  SELECT * FROM user_account WHERE username = user_id;
 END//
 
+DELIMITER ;
 DROP FUNCTION IF EXISTS loginUser;
+DELIMITER //
 CREATE FUNCTION loginUser(username VARCHAR(64), pass VARCHAR(64)) RETURNS INT
 NOT DETERMINISTIC READS SQL DATA
 BEGIN
@@ -16,10 +18,119 @@ BEGIN
  RETURN query_count;
 END//
 
+
+
+DELIMITER ;
 DROP PROCEDURE IF EXISTS insertUser;
+DELIMITER //
 CREATE PROCEDURE insertUser(IN username VARCHAR(64), IN pass VARCHAR(64), IN date DATE)
 BEGIN
  INSERT INTO user_account(user_id, password, reg_date, login_streak) VALUES (username, pass, date, 1);
+ INSERT INTO user_achievements(user_id,a_id, date_unlock) VALUES (username,0,date);
+END//
+
+
+
+### Programming objects based on workout objects
+# Returns a workout if it exists (from name)
+DELIMITER ;
+DROP PROCEDURE IF EXISTS workOutExists;
+DELIMITER //
+CREATE PROCEDURE workOutExists(IN workoutname VARCHAR(20))
+BEGIN
+
+  SELECT * FROM workout WHERE workoutname = name;
+
+END//
+
+# Finds a workout based on the difficulty
+DELIMITER ;
+DROP PROCEDURE IF EXISTS findWorkOutByDifficulty;
+DELIMITER //
+CREATE PROCEDURE findWorkOutByDifficulty(IN difficulty_p INT)
+BEGIN
+
+	SELECT * FROM workout WHERE difficulty_p = diffIculty;
+    
+END//
+
+# Finds a workout based on what equipment is needed
+DELIMITER ;
+DROP PROCEDURE IF EXISTS findWorkOutByEquipment;
+DELIMITER //
+CREATE PROCEDURE findWorkOutByEquipment(IN equipment_p VARCHAR(999))
+BEGIN
+
+	SELECT * FROM workout WHERE equipment LIKE CONCAT('%', equipment_p, '%');
+    
+END//
+
+# Finds a workout based on what the muscle group that is worked out
+DELIMITER ;
+DROP PROCEDURE IF EXISTS findWorkOutByMuscleGroup;
+DELIMITER //
+CREATE PROCEDURE findWorkOutByMuscleGroup(IN muscle_group_p VARCHAR(999))
+BEGIN
+
+	SELECT * FROM workout WHERE muscle_group LIKE CONCAT('%', muscle_group_p, '%');
+    
+END//
+
+# Finds a workout based on what type of exercise
+DELIMITER ;
+DROP PROCEDURE IF EXISTS findWorkOutByExercise;
+DELIMITER //
+CREATE PROCEDURE findWorkOutByExercise(IN exercise_type_p VARCHAR(999))
+BEGIN
+	SELECT * FROM workout WHERE exercise_type LIKE CONCAT('%', exercise_type_p, '%');
+    
+END//
+
+# reads all the achievements shown
+DELIMITER ; 
+DROP PROCEDURE IF EXISTS viewAchievements;
+DELIMITER // 
+CREATE PROCEDURE viewAchievements()
+BEGIN
+	SELECT * FROM achievements;
+END // 
+
+# Gets the achievements earned by a specified user
+DELIMITER ;
+DROP PROCEDURE IF EXISTS findUserAchievements;
+DELIMITER //
+CREATE PROCEDURE findUserAchievements(IN user_p VARCHAR(64))
+BEGIN
+    SELECT user_id, achievements.* FROM user_achievements 
+		JOIN achievements ON user_achievements.a_id = achievements.a_id
+		WHERE user_p = user_id;
+END//
+
+
+DELIMITER ;
+DROP PROCEDURE IF EXISTS addMeal;
+DELIMITER //
+CREATE PROCEDURE addMeal(IN d_description VARCHAR(500), IN cals INT, IN carbs INT, IN protein INT, IN fat INT)
+BEGIN
+	 INSERT INTO meal(description, total_calories, carbs_g, protein_g, fat_g) VALUES (d_description, cals, carbs, protein,fat);
+END//
+
+DELIMITER ;
+DROP PROCEDURE IF EXISTS editMeal;
+DELIMITER //
+CREATE PROCEDURE editMeal(IN id INT, IN d_description VARCHAR(500), IN cals INT, IN carbs INT, IN protein INT, IN fat INT)
+BEGIN 
+	UPDATE meal
+    SET description = d_description, total_calories = cals, carbs_g = carbs, protein_g = protein, fat_g = fat
+    WHERE meal_id = id;
+END//
+
+DELIMITER ;
+DROP PROCEDURE IF EXISTS deleteMeal;
+DELIMITER //
+CREATE PROCEDURE deleteMeal(IN id INT)
+BEGIN 
+	DELETE FROM meal WHERE id = meal_id;
 END//
 
 
@@ -33,3 +144,69 @@ SELECT (loginUser("dannyson900","testing1")) as logged_user;
 SELECT * FROM user_account;
 
 */
+
+/*
+DELIMITER ;
+# Tests related to login management
+DELETE FROM user_account WHERE user_id = "dannyson900";
+INSERT INTO user_account(user_id, password, reg_date, login_streak) VALUES ("dannyson900", "testing1", "2022-04-18",1);
+SELECT * FROM user_account;
+CALL userExists("dannyson900");
+SELECT (loginUser("dannyson900", "test-false")) as logged_user;
+SELECT (loginUser("dannyson900","testing1")) as logged_user;
+SELECT * FROM user_account;
+# Tests related to workouts
+-- workOutExists(...)
+INSERT INTO workout(w_id, name, description, equipment, muscle_group, difficulty, exercise_type)
+	VALUES (0, 'pushup', 'Calisthenics exercise starting from the prone position and done by raising and lowering the body using the arms',
+			'none', 'abdominals, deltoids, pectoral, triceps', '5', 'calisthenics');
+CALL workOutExists('pushup');
+CALL workOutExists('coffee');
+DELETE FROM workout WHERE w_id = 0;
+CALL workOutExists('pushup');
+-- findWorkOutByDifficulty/Equipment/MuscleGroup/Exercise(...)
+INSERT INTO workout(w_id, name, description, equipment, muscle_group, difficulty, exercise_type)
+	VALUES (0, 'pushup', 'Calisthenics exercise starting from the prone position and done by raising and lowering the body using the arms',
+			'none', 'abdominals, deltoids, pectoral, triceps', 5, 'calisthenics');
+INSERT INTO workout(w_id, name, description, equipment, muscle_group, difficulty, exercise_type)
+	VALUES (1, 'test', 'test', 'none', 'none', 0, 'nothing');
+INSERT INTO workout(w_id, name, description, equipment, muscle_group, difficulty, exercise_type)
+	VALUES (2, 'hard_workout_1', 'hardest workout ever', 'none', 'every muscle', 10, 'everything');
+INSERT INTO workout(w_id, name, description, equipment, muscle_group, difficulty, exercise_type)
+	VALUES (3, 'moderate_workout', 'a moderate workout', 'none', 'legs', 5, 'cardio');
+INSERT INTO workout(w_id, name, description, equipment, muscle_group, difficulty, exercise_type)
+	VALUES (4, 'many_equipments', 'a workout with a lot of equipments',
+    'pullup_bar, dumbbell, towel, headband, chicken', 'legs', 5, 'cardio');
+INSERT INTO workout(w_id, name, description, equipment, muscle_group, difficulty, exercise_type)
+	VALUES (5, 'one_equipment', 'a workout with 1 equipment', 'dumbbell', 'arms', 5, 'cardio');
+SELECT * FROM workout;  
+CALL findWorkOutByDifficulty(5);
+CALL findWorkOutByEquipment('dumbbell');
+CALL findWorkOutByMuscleGroup('legs');
+CALL findWorkOutByExercise('cardio');
+DELETE FROM workout WHERE w_id >= 0;  
+# Tests related to achievements
+-- findUserAchievements(...)
+INSERT INTO user_account(user_id, password, reg_date, login_streak) VALUES ("dannyson900", "testing1", "2022-04-18", 1);
+INSERT INTO user_account(user_id, password, reg_date, login_streak) VALUES ("swagman4", "testing3", "2022-04-18", 1);
+INSERT INTO user_account(user_id, password, reg_date, login_streak) VALUES ("obama", "password", "2022-04-18", 1);
+INSERT INTO user_account(user_id, password, reg_date, login_streak) VALUES ("travis", "penutbutter7", "2022-04-18", 100);
+SELECT * FROM user_account;
+INSERT INTO achievements(a_id, name, description, points) VALUES (0, 'test1', 'winrar', 10);
+INSERT INTO achievements(a_id, name, description, points) VALUES (1, 'test2', 'biggerwinrar', 20);
+INSERT INTO achievements(a_id, name, description, points) VALUES (2, 'test3', 'superbwinrar', 100);
+INSERT INTO achievements(a_id, name, description, points) VALUES (3, 'test4', 'hugewinrar', 1000);
+SELECT * FROM achievements;
+INSERT INTO user_achievements(achievement_unlock_id, user_id, a_id, time_unlock) VALUES(0, 'dannyson900', 0, '2022-04-19');
+INSERT INTO user_achievements(achievement_unlock_id, user_id, a_id, time_unlock) VALUES(1, 'dannyson900', 1, '2022-04-20');
+INSERT INTO user_achievements(achievement_unlock_id, user_id, a_id, time_unlock) VALUES(2, 'dannyson900', 2, '2022-04-21');
+INSERT INTO user_achievements(achievement_unlock_id, user_id, a_id, time_unlock) VALUES(3, 'obama', 0, '2022-04-20');
+INSERT INTO user_achievements(achievement_unlock_id, user_id, a_id, time_unlock) VALUES(4, 'dannyson900', 3, '2022-04-21');
+INSERT INTO user_achievements(achievement_unlock_id, user_id, a_id, time_unlock) VALUES(5, 'travis', 2, '2022-04-19');
+SELECT * FROM user_achievements;
+SELECT * FROM user_achievements JOIN achievements ON user_achievements.a_id = achievements.a_id;
+CALL findUserAchievements('dannyson900');
+CALL findUserAchievements('obama');
+CALL findUserAchievements('stacy');
+/*
+
